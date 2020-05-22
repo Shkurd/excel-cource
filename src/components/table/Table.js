e48fe6e7dc70666e15c56e1b143e570aec68a5c9
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-05-22 12:41:01 +0300

    cells select added

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,9 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from './table.template';
 import {resizeHandler} from './table.resize';
-import {shouldResize} from './table.functions';
+import {shouldResize, isCell} from './table.functions';
 import {TableSelection} from './TableSelection';
+import {$} from '../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,16 +17,29 @@ export class Table extends ExcelComponent {
     return createTable(25)
   }
 
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
   init() {
     // не перезатираем init родительского класса тут, для этого используем super
     super.init()
 
-    this.selection = new TableSelection()
+    // console.log($cells.dataset('[data-id]'))
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selection.select($cell)
   }
 
   onMousedown(e) {
     if (shouldResize(e)) {
       resizeHandler(e, this.$root)
+    } else if (isCell(e)) {
+      const target = $(e.target)
+      if (e.shiftKey) {
+        this.selection.selectGroup(target)
+      } else {
+        this.selection.select(target)
+      }
     }
   }
 }
--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -1,14 +1,28 @@
 
 export class TableSelection {
+  // обращаться к className можно через название класса TableSelection.className
+  static className = 'selected'
   constructor($root) {
-
+    this.group = []
   }
 
-  select() {
-    console.log('method select')
+  // $el instanceof DOM === true
+  select($el) {
+    this.clear()
+    this.group.push($el)
+    // $el.addClass('selected')
+    $el.addClass(TableSelection.className)
   }
 
-  selectGroup() {
-    console.log('method selectGroup')
+  clear() {
+    this.group = this.group.forEach(($el)=>{
+      $el.removeClass(TableSelection.className)
+    })
+    this.group = []
+  }
+
+  selectGroup($el) {
+    this.group.push($el)
+    $el.addClass(TableSelection.className)
   }
 }
