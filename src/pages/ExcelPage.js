97f24ee487815e5049b07526dbf0743ada69bf24
Shkurdov <anton.shkurdov@vrpconsulting.com>
2020-06-10 11:46:01 +0300

    acync loader added

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -40,7 +40,13 @@ class LocalStorageClient {
   }
 
   get() {
-    return Promise.resolve(storage(this.name))
+    // return Promise.resolve(storage(this.name))
+    return new Promise(resolve => {
+      const state = storage(this.name)
+      setTimeout(() => {
+        resolve(state)
+      }, 800)
+    })
   }
 }
 
